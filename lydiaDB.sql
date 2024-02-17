abeb3d467120e31b5f6fba41e2fdad6be52f3ffe
kubasov2024 <160057965+kubasov2024@users.noreply.github.com>
2024-02-17 16:25:39 +0300

    Update lydiaDB.sql

--- lydiaDB.sql
+++ lydiaDB.sql
@@ -4,9 +4,9 @@ DEFAULT CHARACTER SET utf8mb4 COLLATE utf8_unicode_ci;
 CHARACTER SET utf8_unicode_ci
 
 CREATE TABLE employees (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(50),
   last_name VARCHAR(50),
-  email VARCHAR(100),
+  email VARCHAR(100) UNIQUE,
   hire_date DATE
 ) ENGINE=MyISAM;
