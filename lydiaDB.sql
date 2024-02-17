dee6607f9643d707a94239debb18c4c943943a61
kubasov2024 <160057965+kubasov2024@users.noreply.github.com>
2024-02-17 16:20:21 +0300

    Update lydiaDB.sql

--- lydiaDB.sql
+++ lydiaDB.sql
@@ -1,7 +1,12 @@
+CREATE database Lydiadb1;
+USE db1;
+DEFAULT CHARACTER SET utf8mb4 COLLATE utf8_unicode_ci;
+CHARACTER SET utf8_unicode_ci
+
 CREATE TABLE employees (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(50),
   last_name VARCHAR(50),
   email VARCHAR(100),
   hire_date DATE
-);
+) ENGINE=MyISAM;
